4dda04f1c6c68d0c1cd98f4de398fc073f077c43
Kateryna Havryna <khavryna@gmail.com>
2025-11-13 17:18:41 +0100

    hW9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,51 @@
-console.log('form');
+
+const STORAGE_KEY = "feedback-form-state";
+
+
+let formData = {
+  email: "",
+  message: "",
+};
+
+
+const form = document.querySelector(".feedback-form");
+
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  try {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email || "";
+    form.elements.message.value = formData.message || "";
+  } catch (error) {
+    console.error("Помилка при читанні сховища:", error);
+  }
+}
+
+
+form.addEventListener("input", (event) => {
+  const { name, value } = event.target;
+  formData[name] = value.trimStart();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const { email, message } = formData;
+
+
+  if (email.trim() === "" || message.trim() === "") {
+    alert("Fill please all fields");
+    return;
+  }
+
+
+  console.log("Form data:", formData);
+
+ 
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: "", message: "" };
+  form.reset();
+});
